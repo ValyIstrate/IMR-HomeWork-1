7edf24c5fec875d0ad4bd22b12a818ade57a399f
Valentin <valentin.istrate26@outlook.com>
2023-10-10 13:47:50 +0300

    push project

---
+++ 2Cactuses/Assets/Fight.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Fight : MonoBehaviour
+{
+    private Animator _mAnimator;
+    public Transform otherCactus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_mAnimator != null)
+        {
+            if (Vector3.Distance(transform.position, otherCactus.position) <= 0.25f)
+            {
+                _mAnimator.SetBool("isAttacking", true);
+            }
+            else
+            {
+                _mAnimator.SetBool("isAttacking", false);
+            }
+        }
+    }
+}
